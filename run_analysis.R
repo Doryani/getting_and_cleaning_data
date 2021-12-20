a84b90118c09aabcc3b0288ec037c1b9872c4b4e
Zyad Allam <zyad.wrk@gmail.com>
2021-12-20 18:58:58 +0300

    added comments to make the code clearer

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+#load library
 library(dplyr)
 filename <- "Coursera_DS3_Final.zip"
 
@@ -12,6 +13,7 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(filename)
 }
 
+#read data
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -21,15 +23,18 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+#merge data
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merged_Data <- cbind(Subject, Y, X)
 
+#get data for mean and std only
 tidy_data <- Merged_Data %>%
 select(subject, code, contains("mean"), contains("std"))
 tidy_data$code <- activities[tidy_data$code, 2]
 
+#appropriate activity names
 names(tidy_data)[2] = "activity"
 names(tidy_data) <- gsub("Acc", "Accelerometer", names(tidy_data))
 names(tidy_data) <- gsub("Gyro", "Gyroscope", names(tidy_data))
@@ -46,10 +51,12 @@ names(tidy_data), ignore.case = TRUE)
 names(tidy_data) <- gsub("angle", "Angle", names(tidy_data))
 names(tidy_data) <- gsub("gravity", "Gravity", names(tidy_data))
 
+#create tidy dataset
 final_data <- tidy_data %>%
     group_by(subject, activity) %>%
     summarise_all(funs(mean))
 write.table(final_data, "final_data.txt", row.name = FALSE)
 
+#view contents to make sure everything runs
 str(final_data)
 final_data
